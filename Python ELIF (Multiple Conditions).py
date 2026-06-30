c862a6de742e1542692e8e304ed50f8730432325
Trika939 <seemaverma3186@gmail.com>
2026-06-30 14:01:18 +0530

    Add files via upload

---
+++ Python ELIF (Multiple Conditions).py
@@ -0,0 +1,197 @@
+         #### Python ELIF (Multiple Conditions) ####
+
+"""
+31. Print day name using day number (1–7).
+
+day=int(input("Enter day number (1-7): "))
+if day==1:
+    print("monday")
+elif day==2:
+    print("tuesday")
+elif day==3:
+    print("wednesday")
+elif day==4:
+    print("thursday")
+elif day==5:
+    print("friday")
+elif day==6:
+    print("saturday")
+elif day==7:
+    print("sunday")
+else:
+    print("Invailed day number")
+      
+
+
+
+32. Print month name using month number.
+
+month=int(input("Enter month 1-12: "))
+if month==1:
+    print("jan")
+elif month==2:
+    print("feb")
+elif month==3:
+    print("mar")
+elif month==4:
+    print("Apr")
+elif month==5:
+    print("may")
+elif month==6:
+    print("jun")
+elif month==7:
+    print("jul")
+elif month==8:
+    print("Aug")
+elif month==9:
+    print("sep")
+elif month==10:
+    print("Oct")
+elif month==11:
+    print("Nov")
+elif month==12:
+    print("Dec")
+else:
+    print("Invailed month")
+
+
+
+
+33. Display grade based on percentage. 
+
+peracentage=float(input("Enter percentage : "))
+if peracentage>=90:
+    print("Grade A ")
+elif peracentage>=75:
+    print("Grade B ")
+elif peracentage>=60:
+    print("Grade C")
+elif peracentage>=45:
+    print("Grade D")
+else:
+    print("Fail")
+
+
+34. Display bonus percentage based on experience years.
+
+experience=int(input("Enter years of experience : "))
+if experience>=10:
+    print("bouns = 25%")
+elif experience>=5:
+    print("bouns = 18%")
+elif experience>=3:
+    print("bouns = 10%")
+else:
+    print("bouns = 5%")
+
+
+35. Identify traffic signal meaning.
+
+signal=input("Enter signal color (Red/yellow/Green) :")
+if signal =="Red":
+    print("stop")
+elif signal =="yellow":
+    print("wait")
+elif signal =="Green":
+    print("Go")
+else:
+    print("Invailed signal")
+
+
+
+36. Categorize temperature as Cold / Warm / Hot.
+
+temp=float(input("Enter temperature : "))
+if temp<=15:
+    print("cold")
+elif temp<=30:
+    print("warm")
+else:
+    print("hot")
+
+
+
+37. Categorize employee based on salary range.
+
+salary=int(input("Enter employee salary : "))
+if salary<=20000:
+    print("Low salary")
+elif salary<=40000:
+    print("medium salary")
+elif salary<=90000:
+    print("High salary")
+else:
+    print("very high salary")
+
+
+
+38. Print discount percentage based on purchase amount.
+
+amount=float(input("Enter purchase amount :"))
+if amount>=5000:
+    print("discount = 20%")
+elif amount>=3000:
+    print("discount = 10%")
+else:
+    print("discount = 5%")
+
+
+
+39. Identify number type: single-digit / double-digit / multi-digit.    
+
+num=int(input("Enter a number : "))
+if num>=0 and num<=9:
+    print("single-digit")
+elif num>=10 and num<=99:
+    print("double-digit")
+else:
+    print("multi-digit")
+
+
+40. Assign performance rating: Poor / Average / Good / Excellent.
+
+rating=float(input("Enter performance score : "))
+if rating>=90:
+    print("Excellent")
+elif rating>=75:
+    print("Good")
+elif rating>=50:
+    print("Average")
+else:
+    print("poor")
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
